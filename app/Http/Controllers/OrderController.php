f607c6faa0f29a37ac3560efd5eb0f16e671c49b
Kevinwong206 <kevinwong206@gmail.com>
2022-10-07 11:36:43 +0800

    Completed Authentication Function
    - Completed Sign In function
    - Completed Sign Up function
    - Completed authentication for sign in and sign up
    - Completed forgot password feature

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Order;
+use DB;
+
+class OrderController extends Controller
+{
+    /**
+     * Show User's Profile Page
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function orderHistoryView()
+    {
+        $orders = DB::table('orders')
+                ->orderBy('created_at', 'DESC')
+                ->paginate(10);
+        return view('order.orderHistory')->with(compact('orders'));
+    }
+
+    /**
+     * Show User's Profile Page
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function orderDetailsView(Request $request)
+    {
+        $order = Order::find($request->orderID);
+        $orderItems = DB::table('orderitem')
+                    ->select('orderitem.qty', 'orderitem.ISBN13', 'stock.coverImg', 'stock.bookTitle', 'stock.retailPrice')
+                    ->join('stock', 'orderitem.ISBN13', '=', 'stock.ISBN13')
+                    ->where('orderitem.orderID', '=', $order->orderID)
+                    ->get();
+        return view('order.orderDetails')->with(compact('order', 'orderItems'));
+    }
+}
